e42504747f03a9c46e0ab75c706e7c2050a746f4
Caesar0815 <gingermk007@gmail.com>
2020-07-23 01:01:02 +0200

    added addCharacter added updateCharacterPosition

--- resolvers.js
+++ resolvers.js
@@ -1,4 +1,4 @@
-import { ObjectID } from "mongodb";
+import { ObjectId } from "mongodb";
 import { GraphQLScalarType } from "graphql";
 import { Kind } from "graphql/language";
 
@@ -12,7 +12,7 @@ export const resolvers = {
         // Players
         allPlayers: async (parent, args, { db }) => await db.collection("players").find().toArray(),
         totalPlayers: async (parent, args, { db }) => await db.collection("players").estimatedDocumentCount(),
-        getPlayerByID: async (parent, args, { db }) => await db.collection("players").findOne(ObjectID(args.id)),
+        getPlayerByID: async (parent, args, { db }) => await db.collection("players").findOne(ObjectId(args.id)),
 
         // Characters
         allCharacters: async (parent, args, { db }) => {
@@ -26,20 +26,35 @@ export const resolvers = {
                     }
                 }
             ]).toArray();
-            console.log(characters);
+            console.log(characters)
             return characters;
 
         },
         totalCharacters: async (parent, args, { db }) => await db.collection("characters").estimatedDocumentCount(),
-        getCharacterByID: async (parent, args, { db }) => await db.collection("characters").findOne(ObjectID(args.id)),
+        getCharacterByID: async (parent, args, { db }) => (await db.collection("characters").aggregate([
+            {
+                $match: {
+                    _id: ObjectId(args.id)
+                }
+            },{
+                "$lookup": {
+                    "from": "players",
+                    "localField": "players",
+                    "foreignField": "_id",
+                    "as": "players"
+                }
+
+            }
+        ]).toArray())[0],
 
         // Measurements
         allMeasurements: async (parent, args, { db }) => await db.collection("measurement").find().toArray(),
         totalMeasurements: async (parent, args, { db }) => await db.collection("measurement").estimatedDocumentCount(),
-        getMeasurementsByID: async (parent, args, { db }) => await db.collection("measurement").findOne(ObjectID(args.id))
+        getMeasurementsByID: async (parent, args, { db }) => await db.collection("measurement").findOne(ObjectId(args.id))
 
-    },/*
+    },
     Mutation: {
+        /*
         addPhoto: async (parent, args, { db }) => {
             let photo = {
                 ...args,
@@ -49,8 +64,30 @@ export const resolvers = {
             photo.id = insertedIds;
 
             return photo;
-        }
-    },*/
+        }*/
+        addCharacter: async (parent, args, { db }) => {
+            let character = {
+                ...args,
+            };
+
+            character.token = character.token.href;
+            for (let i in character.players) {
+                character.players[i] = ObjectId(character.players[i]);
+            }
+
+            const { insertedIds } = await db.collection("characters").insertOne(character);
+            character.id = insertedIds;
+
+            return character;
+        },
+        updateCharacterPosition: async (parent, args, { db }) => (await db.collection("characters").findOneAndUpdate(
+                { _id: ObjectId(args.id)},
+                {
+                    $set: { "pos.point.x": args.x, "pos.point.y": args.y},
+                },
+            {returnOriginal: false}
+            )).value
+    },
 
     Player: {
         id: parent => parent.id || parent._id,
@@ -63,11 +100,20 @@ export const resolvers = {
     },
     Character: {
         id: parent => parent.id || parent._id,
-        players: async (parent, args, { db }) => await db.collection("players").find({
-            "_id": {
-                $in: parent.players.map(p => ObjectID(p))
-            }
-        }).toArray()
+        players: async (parent, args, { db }) => {
+            if (parent.players.length === 0)
+                return parent.players;
+
+            let res =  typeof parent.players[0].name != "undefined" ? parent.players :
+            await db.collection("players").find({
+                "_id": {
+                    $in: parent.players.map(p => {
+                        return ObjectId(p);
+                    })
+                }
+            }).toArray();
+            return res
+        }
     },
     PositionSquare: {
         rot: parent => parent.rot || 0,
