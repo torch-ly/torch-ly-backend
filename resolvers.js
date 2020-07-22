350d97bb4d811d9da4953e4b09d10d49d1ccfbfe
Caesar0815 <gingermk007@gmail.com>
2020-07-22 22:56:51 +0200

    finished allCharacters query

--- resolvers.js
+++ resolvers.js
@@ -15,7 +15,21 @@ export const resolvers = {
         getPlayerByID: async (parent, args, { db }) => await db.collection("players").findOne(ObjectID(args.id)),
 
         // Characters
-        allCharacters: async (parent, args, { db }) => await db.collection("characters").find().toArray(),
+        allCharacters: async (parent, args, { db }) => {
+            let characters = await db.collection("characters").aggregate([
+                {
+                    "$lookup": {
+                        "from": "players",
+                        "localField": "players",
+                        "foreignField": "_id",
+                        "as": "players"
+                    }
+                }
+            ]).toArray();
+            console.log(characters);
+            return characters;
+
+        },
         totalCharacters: async (parent, args, { db }) => await db.collection("characters").estimatedDocumentCount(),
         getCharacterByID: async (parent, args, { db }) => await db.collection("characters").findOne(ObjectID(args.id)),
 
@@ -39,7 +53,25 @@ export const resolvers = {
     },*/
 
     Player: {
-        id: parent => parent.id || parent._id
+        id: parent => parent.id || parent._id,
+        gm: parent => process.env.GM.includes(parent.id || parent._id),
+        characters: async (parent, args, { db }) => await db.collection("characters").find({
+            players: {
+                $in: [parent.id || parent._id]
+            }
+        }).toArray()
+    },
+    Character: {
+        id: parent => parent.id || parent._id,
+        players: async (parent, args, { db }) => await db.collection("players").find({
+            "_id": {
+                $in: parent.players.map(p => ObjectID(p))
+            }
+        }).toArray()
+    },
+    PositionSquare: {
+        rot: parent => parent.rot || 0,
+        size: parent => parent.size || 1
     },
     URL: URLResolver,
     JSON: JSONResolver
