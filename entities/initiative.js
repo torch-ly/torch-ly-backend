f6d11f854bfd5d74ec9bab15e25e9f0e584182c6
Caesar0815 <gingermk007@gmail.com>
2020-09-06 16:06:35 +0200

    added initiative backend

--- entities/initiative.js
+++ entities/initiative.js
@@ -1,15 +1,29 @@
 import {pubsub} from "../index";
 
-export let initiative = [];
+export let initiative = [
+    {
+        id: "5f298f49da955f54d460a603",
+        value: 1
+    },
+    {
+        id: "5f4e84a100e1a15b8c582561",
+        value: 3
+    },
+    {
+        id: "5f4ec32d84e88f4662e6994c",
+        value: 2
+    }
+];
 
 export const queries = {
-    getInitiative: () => {
-        return initiative;
-    }
+    getInitiative: () => ({
+        order: initiative
+    }),
 };
 
 export const mutations = {
     updateInitiative: (parent, args) => {
+        console.log(args.order)
         initiative = args.order;
 
         pubsub.publish("initiative-update", {updateInitiative: {order: args.order}});
