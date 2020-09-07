670d920e8be25d32a6ba183ee78cee1ac5e289db
Caesar0815 <gingermk007@gmail.com>
2020-09-07 13:11:20 +0200

    changed default value of initiative variable

--- entities/initiative.js
+++ entities/initiative.js
@@ -1,19 +1,6 @@
 import {pubsub} from "../index";
 
-export let initiative = [
-    {
-        id: "5f298f49da955f54d460a603",
-        value: 1
-    },
-    {
-        id: "5f4e84a100e1a15b8c582561",
-        value: 3
-    },
-    {
-        id: "5f4ec32d84e88f4662e6994c",
-        value: 2
-    }
-];
+export let initiative = [];
 
 export const queries = {
     getInitiative: () => ({
