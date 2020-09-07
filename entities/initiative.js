faa6647cf31ed104e6b51311157ac22d6c4f2fa3
EricHier <developer.erichier@gmail.com>
2020-09-07 16:11:43 +0200

    only send ini updates when it changed

--- entities/initiative.js
+++ entities/initiative.js
@@ -1,4 +1,5 @@
 import {pubsub} from "../index";
+import { default as hash } from "object-hash";
 
 export let initiative = [];
 
@@ -10,10 +11,14 @@ export const queries = {
 
 export const mutations = {
     updateInitiative: (parent, args) => {
+
+        let updated = hash(initiative) !== hash(args.order);
+
         console.log(args.order)
         initiative = args.order;
 
-        pubsub.publish("initiative-update", {updateInitiative: {order: args.order}});
+        if (updated)
+            pubsub.publish("initiative-update", {updateInitiative: {order: args.order}});
 
         return {order: args.order};
     },
