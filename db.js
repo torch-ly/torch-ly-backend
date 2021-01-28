cc5f56cc832acb23b21a0f25e638d0890d8f6757
EricHier <developer.erichier@gmail.com>
2021-01-28 11:13:18 +0100

    allowing mongodb connection strings instead of user + password now

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 import { MongoClient } from "mongodb";
 
-export async function setupDB () {
+export async function setupDB() {
     let user = process.env.DB_USER, pwd = process.env.DB_PASSWORD;
 
-    const mongoURL = "mongodb://" + user + ":" + pwd + "@" + process.env.DB_HOST + "/torch-ly-backend?authSource=admin";
+    const mongoURL = process.env.DB || "mongodb://" + user + ":" + pwd + "@" + process.env.DB_HOST + "/torch-ly-backend?authSource=admin";
+
+    console.log(mongoURL);
 
     const mongoClient = await MongoClient.connect(mongoURL, { useUnifiedTopology: true });
 
