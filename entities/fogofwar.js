706a3bc0cd08bc5f645c964fd5e9ec7674342a51
Ondolin <developer@marvin-kaiser.eu>
2021-03-09 21:41:18 +0100

    added add and remove fog of war polygons functions

--- entities/fogofwar.js
+++ entities/fogofwar.js
@@ -1,5 +1,6 @@
 import {fileContent, setFileContent, subscribeOnFileChange} from "../file-handler";
 import {pubsub} from "../index";
+import uniqid from "uniqid";
 
 // will be called for all background layer updates not caused by the background layer mutations
 function updateFogOfWar() {
@@ -27,7 +28,22 @@ export const queries = {
 
 export const mutations = {
     updateFogOfWar: (parent, args) => {
+        let objects = args.json;
+
+        objects.forEach((obj) => obj._id = uniqid());
+
         saveUpdatedFogOfWar(args.json);
+        return {polygons: getFogOfWar()};
+    },
+    addFogOfWarObject: (parent, args) => {
+
+        args.json._id = uniqid();
+
+        saveUpdatedFogOfWar([...getFogOfWar(), args.json]);
+        return {polygons: getFogOfWar()};
+    },
+    removeFogOfWarObject: (parent, args) => {
+        saveUpdatedFogOfWar(getFogOfWar().filter(obj => obj._id !== args.id));
         return {polygons: getFogOfWar()};
     },
 };
